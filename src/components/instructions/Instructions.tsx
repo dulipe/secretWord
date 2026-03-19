9f97b2bb04c4947b3bb2e32bc190394b551b9a32
dulipe <filipe_opc2@hotmail.com>
2026-03-19 05:08:55 -0300

    fix: update component colors to match new background

--- src/components/instructions/Instructions.tsx
+++ src/components/instructions/Instructions.tsx
@@ -1,6 +1,6 @@
 const Instructions = () => {
     return (
-      <section className="mt-8 mb-10 w-full px-6 text-x1 text-gray-600">
+      <section className="mt-8 mb-10 w-full px-6 text-x1 text-black">
         <h2 className="text-x1 font-bold text-gray-800 mb-3">
           Como jogar
         </h2>
