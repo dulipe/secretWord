0a18a18b3d3563d1504d5b4d8d482167bbb66ef3
dulipe <filipe_opc2@hotmail.com>
2026-03-19 06:00:29 -0300

    feat: update meta tags

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,9 +2,18 @@ const Footer = () => {
   return (
     <footer className="w-full px-4 py-3 border-t-3 border-white flex items-center justify-center mt-auto relative bg-neutral-900">
       <p className="text-sm text-white">
-        © {new Date().getFullYear()} Filipe Santos. All rights reserved.
+        © {new Date().getFullYear()} Filipe Santos · Inspired by{" "}
+        <a
+          href="https://www.geniol.com.br/palavras/palavras-secretas/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline hover:text-gray-800 transition-colors duration-200"
+        >
+          Secret Words
+        </a>{" "}
+        of Geniol
       </p>
-    </footer>
+    </footer >
   );
 };
 
